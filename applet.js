d0c45566a9d52096a7942322a6261d865fa3822c
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2015-09-10 23:42:42 +0200

    Applet-aren bidea eskuratzeko modu sinpleago bat aurkitu dut

--- applet.js
+++ applet.js
@@ -3,24 +3,22 @@
  */
 const Applet = imports.ui.applet;
 
-const AppletDir = imports.ui.appletManager.appletMeta["kaixo-cinnamon-applet@aldatsa"].path;
-
-function KaixoMundua(orientation, panel_height, instance_id) {
+function KaixoMundua(metadata, orientation, panel_height, instance_id) {
     this._init(orientation, panel_height, instance_id);
 }
 
 KaixoMundua.prototype = {
     __proto__: Applet.IconApplet.prototype,
 
-    __init__: function(orientation, panel_height, instance_id) {
+    __init__: function(metadata, orientation, panel_height, instance_id) {
         Applet.IconApplet.prototype.__init__.call(this, orientation, panel_height, instance_id);
 
-        // Ikonoa "Gehitu applet-ak panelari" menuan agertzen da baina ez erretiluan. Zergatik?
-        this.set_applet_icon_path(AppletDir + "/icon.png");
+        // Ikonoa "Gehitu applet-ak panelari" menuan eta "Honi buruz..."-en agertzen da baina ez erretiluan. Zergatik?
+        this.set_applet_icon_path(metadata.path + "/icon.png");
         this.set_applet_tooltip(_("Kaixo mundua"));
     }
 };
 
 function main(metadata, orientation, panel_height, instance_id) {
-    return new KaixoMundua(orientation, panel_height, instance_id);
+    return new KaixoMundua(metadata, orientation, panel_height, instance_id);
 }
