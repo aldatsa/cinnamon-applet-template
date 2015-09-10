275a4bb0c64cd0fcf18523bc0709a7f1e4c1a820
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2015-09-10 23:28:46 +0200

    'Gehitu applet-ak panelari' menuan ikonoa bistaratzea lortu

--- applet.js
+++ applet.js
@@ -3,6 +3,8 @@
  */
 const Applet = imports.ui.applet;
 
+const AppletDir = imports.ui.appletManager.appletMeta["kaixo-cinnamon-applet@aldatsa"].path;
+
 function KaixoMundua(orientation, panel_height, instance_id) {
     this._init(orientation, panel_height, instance_id);
 }
@@ -13,7 +15,8 @@ KaixoMundua.prototype = {
     __init__: function(orientation, panel_height, instance_id) {
         Applet.IconApplet.prototype.__init__.call(this, orientation, panel_height, instance_id);
 
-        this.set_applet_icon_name("kaixo-mundua");
+        // Ikonoa "Gehitu applet-ak panelari" menuan agertzen da baina ez erretiluan. Zergatik?
+        this.set_applet_icon_path(AppletDir + "/icon.png");
         this.set_applet_tooltip(_("Kaixo mundua"));
     }
 };
