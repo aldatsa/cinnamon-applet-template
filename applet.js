b042c61d95f4555dd22732a925abbc06dbd16fa9
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2015-09-16 20:34:08 +0200

    Metodo baten izena zuzendu, parametro bat falta zuen eta applet-aren direktorioa eskuratzeko modua aldatu

--- applet.js
+++ applet.js
@@ -2,19 +2,20 @@
  * /usr/share/cinnamon/js/
  */
 const Applet = imports.ui.applet;
+const APPLET_UUID = "kaixo-cinnamon-applet@aldatsa";
+const APPLET_DIR = imports.ui.appletManager.appletMeta[APPLET_UUID].path;
 
 function KaixoMundua(metadata, orientation, panel_height, instance_id) {
-    this._init(orientation, panel_height, instance_id);
+    this._init(metadata, orientation, panel_height, instance_id);
 }
 
 KaixoMundua.prototype = {
     __proto__: Applet.IconApplet.prototype,
 
-    __init__: function(metadata, orientation, panel_height, instance_id) {
-        Applet.IconApplet.prototype.__init__.call(this, orientation, panel_height, instance_id);
+    _init: function(metadata, orientation, panel_height, instance_id) {
+        Applet.IconApplet.prototype._init.call(this, orientation, panel_height, instance_id);
 
-        // Ikonoa "Gehitu applet-ak panelari" menuan eta "Honi buruz..."-en agertzen da baina ez erretiluan. Zergatik?
-        this.set_applet_icon_path(metadata.path + "/icon.png");
+        this.set_applet_icon_path(APPLET_DIR + "/icon.png");
         this.set_applet_tooltip(_("Kaixo mundua"));
     }
 };
